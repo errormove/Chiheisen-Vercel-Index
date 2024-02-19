fba315298b9a599389fe995c2c7c213dc45dcf0f
errormove <111301194+errormove@users.noreply.github.com>
2024-02-20 02:46:33 +0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,15 +45,15 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:hmmodsofficial@gmail.com',
+  email: "",
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/Chiheisen-Union/Chiheisen-Vercel-Index',
+      name: 'NeXuS',
+      link: 'https://www.youtube.com',
     },
   ],
 
