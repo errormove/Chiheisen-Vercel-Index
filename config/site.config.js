4e959d494982b730f61fc19999694db541782484
errormove <111301194+errormove@users.noreply.github.com>
2024-02-20 02:50:43 +0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Chiheisen Vercel Index",
+  title: "NexuS",
 
   // The folder that you are to share publicly with Chiheisen-Vercel-Index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/Chiheisen-Union/Chiheisen-Vercel-Index" target="_blank" rel="noopener noreferrer">Chiheisen-Vercel-Index</a>. Made with ❤ by Chiheisen.',
+    'Powered by <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">NexuS-HUB</a>. Made with ❤ by CodeX.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
